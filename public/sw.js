54b34807add4c18e745dfdb6e89d29772552a4c9
null <null>
2025-12-21 22:37:08 -0300

    fix build

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-// Este es un Service Worker básico para que Chrome reconozca la App como instalable
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    // Por ahora no hace nada, pero debe estar presente
-    e.respondWith(fetch(e.request));
-  });
+const CACHE_NAME = 'gym-pro-v1';
+
+// Se ejecuta cuando se instala la app
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+// Se ejecuta cuando la app toma el control
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Obligatorio para que Chrome detecte que la app funciona "offline"
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
